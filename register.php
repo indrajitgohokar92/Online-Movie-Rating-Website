837085378cd29686388dffd5bddb479ceecfeba0
Vipul Jarmale <vipulnj512@gmail.com>
2017-04-03 18:27:10 -0500

    store username, password in users table

--- register.php
+++ register.php
@@ -20,7 +20,6 @@
 	<script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
 	<script type="text/javascript" src="js/bootstrap-datepicker.js"></script>
 	<script type="text/javascript" src="js/validate.js"></script>
-	<!-- <script type="text/javascript" src="js/register.js"></script> -->
 	<link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:100,200,300,400,500,600,700,800,900' rel='stylesheet' type='text/css'>
 	<script>
 		$(function () {
