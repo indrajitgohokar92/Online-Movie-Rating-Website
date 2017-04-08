d6f2284aadefcb399a394d8418b67b882f542b28
Indrajit Gohokar <indrajit.gohokar@gmail.com>
2017-04-08 14:27:35 -0500

    Changes to search movies

--- db/searchmovies.php
+++ db/searchmovies.php
@@ -8,38 +8,37 @@ $dbcon = new mysqli("localhost", "root", "root", "moviedb");
 // echo $movie_category."<br>";
 if(isset($search_term) && isset($movie_category)){
   if(($search_term == "SearchMovies...") && ($movie_category == "all")){
-		$sql = "select movie_id, img_location from movies;";
+		$querymovie = "select movie_id, img_location from movies;";
 	} elseif(($search_term == "SearchMovies...") && !($movie_category == "all")) {
-    $sql = "select movies.movie_id, movies.img_location from movies,categories,movies_categories where
+    $querymovie = "select movies.movie_id, movies.img_location from movies,categories,movies_categories where
             movies.movie_id=movies_categories.movie_id and categories.category_id=movies_categories.category_id
             and categories.category_name='".$movie_category."';";
 	} elseif(!($search_term == "SearchMovies...") && ($movie_category == "all")) {
-    $sql = "select movies.movie_id, movies.img_location from movies,categories,movies_categories where
+    $querymovie = "select movies.movie_id, movies.img_location from movies,categories,movies_categories where
             movies.movie_id=movies_categories.movie_id and categories.category_id=movies_categories.category_id
             and movies.movie_title LIKE '%$search_term%';";
 	} else {
-    $sql = "select movies.movie_id, movies.img_location from movies,categories,movies_categories where
+    $querymovie = "select movies.movie_id, movies.img_location from movies,categories,movies_categories where
             movies.movie_id=movies_categories.movie_id and categories.category_id=movies_categories.category_id
             and categories.category_name='".$movie_category."' and movies.movie_title LIKE '%$search_term%';";
 	}
-  echo $sql."<br>";
-  $result = mysqli_query($dbcon, $sql);
+  echo $querymovie;
+  $result = mysqli_query($dbcon, $querymovie);
   $rows = array();
   if (mysqli_num_rows($result) > 0) {
-        while($row = mysqli_fetch_assoc($result))
-        {
-          $movietitle = $row['movie_id'];
-          $location = $row['img_location'];
-          $rows[] = array("movie_title" => $movietitle, "movie_location" => $location,);
-        }
+    while($row = mysqli_fetch_assoc($result))
+    {
+      $movietitle = $row['movie_id'];
+      $location = $row['img_location'];
+      $rows[] = array("movie_title" => $movietitle, "movie_location" => $location,);
+    }
     session_start();
     $_SESSION['movie_array'] = json_encode($rows);
-    $port = $_SERVER['SERVER_PORT'];
-    $locationUrl = "http://localhost:".$port."/getmovies.php";
-    header("Location:".$locationUrl);
-    exit();
-  } else {
-    echo "failure in getting result";
+    mysqli_close($dbcon);
   }
 }
+$port = $_SERVER['SERVER_PORT'];
+$locationUrl = "http://localhost:".$port."/getmovies.php";
+header("Location:".$locationUrl);
+exit();
 ?>
